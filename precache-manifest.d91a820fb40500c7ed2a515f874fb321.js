e63c05dc00dbbcb40db9656b9287d68625293ab0
Emir Karadag <ekaradag90@gmail.com>
2019-02-09 21:52:17 +0100

    Updates

---
+++ precache-manifest.d91a820fb40500c7ed2a515f874fb321.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bae0e4ee550f75cbcd74",
+    "url": "/myportfolio/static/css/main.069b992b.chunk.css"
+  },
+  {
+    "revision": "bae0e4ee550f75cbcd74",
+    "url": "/myportfolio/static/js/main.bae0e4ee.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/myportfolio/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "795b5e3dbd8c04ed01b5aaa2c4409688",
+    "url": "/myportfolio/index.html"
+  }
+];
